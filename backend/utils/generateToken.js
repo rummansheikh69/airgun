cd76e585913c936ca26e30ecc728240bd358a71d
rummansheikh69 <rummansheikh2004@gmail.com>
2026-03-11 01:16:13 +0600

    s

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 10 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    // sameSite: "none",
+    sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
   });
 };
